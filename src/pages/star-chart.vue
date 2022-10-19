f7af27176bcd983f6e7de4db3ed6c236925ae828
Dirk Arends <dirk@arends.com.au>
2022-10-19 16:44:08 +1100

    WIP

---
+++ src/pages/star-chart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="height: 100%; width: 100%">
+    <client-only>
+      <LcarsStarChart :type="starChartType" />
+    </client-only>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      starChartType: Math.random() > 0.5 ? "nav" : "planet",
+    };
+  },
+  computed: {},
+  mounted() {
+    // this.$setBreadcrumbs([{ to: null, label: "Home" }]);
+  },
+};
+</script>
